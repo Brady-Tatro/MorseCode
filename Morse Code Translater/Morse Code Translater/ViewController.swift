fa433b37309a33e22e78474c0f6c5818cfb078ab
Brady Tatro <bradytatro@gmail.com>
2017-04-22 18:20:59 -0400

    started project

---
+++ Morse Code Translater/Morse Code Translater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Morse Code Translater
+//
+//  Created by Brady Tatro on 4/22/17.
+//  Copyright © 2017 Brady Tatro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
